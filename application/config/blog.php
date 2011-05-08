28748277b5a050216113bdb880030223e2e040d6
Laurence Dawson <loljdawson@gmail.com>
2011-05-08 03:18:24 +0100

    Fixed the default values of blog variables

--- application/config/blog.php
+++ application/config/blog.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['blog_title'] = "Example Blog";
-$config['tagline'] = "Just another embr blog.";
+$config['tagline'] = "Insert witty tagline here.";
 $config['first_name'] = "example_first";
 $config['last_name'] = "example_second";
 $config['contact_email'] = "example@example.com";
