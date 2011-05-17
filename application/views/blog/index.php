0025a1385b04603251246c82e7cf390f531195a7
Laurence Dawson <loljdawson@gmail.com>
2011-05-17 01:11:33 +0100

    Fixed image refs

--- application/views/blog/index.php
+++ application/views/blog/index.php
@@ -1,6 +1,6 @@
 <div class="tools">
-  <div class="toolbox"><a href="<?=base_url()?>customize"><img src="http://localhost/embr/img/customize.png"></a></div>
-  <div class="toolbox"><a href="<?=base_url()?>new_post"><img src="http://localhost/embr/img/new.png"></a></div>
+  <div class="toolbox"><a href="<?=base_url()?>customize"><img src="<?=base_url()?>/img/customize.png"></a></div>
+  <div class="toolbox"><a href="<?=base_url()?>new_post"><img src="<?=base_url()?>/img/new.png"></a></div>
 </div>
 
 <div class="posts">
